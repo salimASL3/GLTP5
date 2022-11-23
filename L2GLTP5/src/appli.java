f358200812f5f167dfde56a4597261b987b380ad
PC <PC@LAPTOP-JH515NOA>
2022-11-23 14:55:51 +0100

    Version initiale du projet

---
+++ L2GLTP5/src/appli.java
@@ -0,0 +1,27 @@
+// appli demo pour TP sur Git
+public class appli {
+
+	public static void main(String[] args) {
+		vecteur v1 = new vecteur(3,5);
+		vecteur v2 = new vecteur(-3,-5);
+		
+		// dev/1
+		System.out.println("v1 = " + v1);
+		System.out.println("v2 = " + v2);
+
+		// dev/2
+		System.out.println("||v1|| = " + v1.norme());
+		System.out.println("||v2|| = " + v2.norme());
+		
+		// dev/3
+		vecteur v3 = v1.plus(v2);
+		System.out.println("v3 = " + v3);
+
+		// master/1
+		int res = 0;
+		// res =  v1.getx() * v2.getx();
+		// res += v1.gety() * v2.gety();
+		System.out.println("v1.v2 = " + res);
+	}
+
+}
---
+++ L2GLTP5/src/vecteur.java
@@ -0,0 +1,34 @@
+
+public class vecteur {
+	
+	private int x=0;
+	private int y=0;
+	
+	public vecteur(int i, int j) {
+		// modif dev/1
+		//this.x = i;
+		//this.y = j;
+	}
+	
+	public double norme() {
+		double norme = 0.0;
+		// modif dev/2
+	    //norme = Math.sqrt(this.x * this.x + this.y * this.y);
+		return norme;
+	}
+	
+	public vecteur plus(vecteur vin) {
+		int xout = 0;
+		int yout = 0;
+		// modif dev/3
+		//xout = this.x + vin.x;
+		//yout = this.y + vin.y;
+		vecteur vout = new vecteur(xout, yout);
+		return vout;
+	}
+
+	public int getx() { return this.x; };
+	public int gety() { return this.y; };
+	public String toString() { return "("+this.x+" , "+this.y+")"; }
+	
+}
